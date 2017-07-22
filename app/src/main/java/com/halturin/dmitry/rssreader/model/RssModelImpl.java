7614c758ea833df132fa55e075702c175997a2d7
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2017-07-22 15:05:58 +0300

    up-to-date rss model implementation

--- app/src/main/java/com/halturin/dmitry/rssreader/model/RssModelImpl.java
+++ app/src/main/java/com/halturin/dmitry/rssreader/model/RssModelImpl.java
@@ -1,5 +1,7 @@
 package com.halturin.dmitry.rssreader.model;
 
+import android.support.annotation.NonNull;
+
 import com.einmalfel.earl.EarlParser;
 import com.einmalfel.earl.Feed;
 import com.einmalfel.earl.Item;
@@ -12,9 +14,12 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.DataFormatException;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -27,6 +32,8 @@ import rx.Observable;
 
 public class RssModelImpl implements RssModel {
 
+    private RealmConfiguration realmConfiguration = null;
+
 //==================================================================================================
 //    Class Constructor
 //==================================================================================================
@@ -34,128 +41,255 @@ public class RssModelImpl implements RssModel {
     public RssModelImpl(){
     }
 
+    public RssModelImpl(@NonNull final String modelName){
+        realmConfiguration = new RealmConfiguration.Builder()
+            .name(modelName)
+            .build();
+    }
+
 //==================================================================================================
 //    Class Methods
 //==================================================================================================
 
-    private Realm getInstance(){
-        return DatabaseModule.getInstance();
+    private Realm getRealmInstance(){
+        Realm realm;
+
+        if(realmConfiguration != null){
+            realm = Realm.getInstance(realmConfiguration);
+        }else{
+            realm = DatabaseModule.getInstance();
+        }
+
+        return realm;
     }
 
-    private long copyFeedToRealm(Feed feed, String url, Realm realm){
+    private FeedEntity getActiveFeedEntity(){
+        Realm realm = getRealmInstance();
+
         FeedEntity feedEntity = realm.where(FeedEntity.class)
-            .equalTo("url", url)
+            .equalTo("isActive", true)
             .findFirst();
 
-        realm.beginTransaction();
-
-        // it is new feed entity in realm
-        if(feedEntity == null){
-            feedEntity = new FeedEntity();
-
-            feedEntity.autoIncrementId(realm);
-
-            feedEntity.setUrl(url);
-        }
-
-        long feedId = feedEntity.getId();
-
-        feedEntity.setAuthor(feed.getAuthor());
-        feedEntity.setTitle(feed.getTitle());
-        feedEntity.setDescription(feed.getDescription());
-        feedEntity.setLink(feed.getLink());
-        feedEntity.setImage(feed.getImageLink());
-        feedEntity.setDate(feed.getPublicationDate());
-        feedEntity.setCopyright(feed.getCopyright());
-
-        realm.copyToRealmOrUpdate(feedEntity);
-        realm.commitTransaction();
-
-        return feedId;
+        return feedEntity;
     }
 
-    private void copyItemToRealm(Item item, long feedId, Realm realm){
-        String itemId = item.getId();
-        ItemEntity itemEntity = realm.where(ItemEntity.class)
-            .equalTo("feedId", feedId)
-            .equalTo("itemId", itemId)
-            .findFirst();
+    private void clearActiveFlagFromAllFeedEntities(){
+        Realm realm = getRealmInstance();
 
-        realm.beginTransaction();
+        try{
+            RealmResults<FeedEntity> feedEntities = realm.where(FeedEntity.class)
+                .equalTo("isActive", true)
+                .findAll();
 
-        // it is new item entity in realm
-        if(itemEntity == null){
-            itemEntity = new ItemEntity();
+            for(FeedEntity feedEntity : feedEntities){
+                realm.beginTransaction();
 
-            itemEntity.autoIncrementId(realm);
+                feedEntity.setActive(false);
 
-            itemEntity.setFeedId(feedId);
-            itemEntity.setItemId(itemId);
+                realm.commitTransaction();
+            }
+        }finally{
+            realm.close();
         }
-
-        itemEntity.setAuthor(item.getAuthor());
-        itemEntity.setTitle(item.getTitle());
-        itemEntity.setDescription(item.getDescription());
-        itemEntity.setLink(item.getLink());
-        itemEntity.setImage(item.getImageLink());
-        itemEntity.setDate(item.getPublicationDate());
-
-        realm.copyToRealmOrUpdate(itemEntity);
-        realm.commitTransaction();
     }
 
-    private void clearRealmFromFeed(String url, Realm realm){
-        RealmResults<FeedEntity> feedEntities = realm.where(FeedEntity.class)
-            .equalTo("url", url)
-            .findAll();
+    private void setActiveFlagForFeedEntity(final String url){
+        Realm realm = getRealmInstance();
 
-        for(FeedEntity feedEntity : feedEntities){
+        try{
+            FeedEntity feedEntity = realm.where(FeedEntity.class)
+                .equalTo("url", url)
+                .findFirst();
+
+            realm.beginTransaction();
+
+            // it is new feed entity in realm
+            if(feedEntity == null){
+                feedEntity = new FeedEntity();
+
+                feedEntity.autoIncrementId(realm);
+
+                feedEntity.setUrl(url);
+            }
+
+            feedEntity.setActive(true);
+
+            realm.copyToRealmOrUpdate(feedEntity);
+            realm.commitTransaction();
+        }finally{
+            realm.close();
+        }
+    }
+
+    private InputStream getFeedStream(final String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        InputStream stream = response.body().byteStream();
+
+        return stream;
+    }
+
+    private void copyFeedToRealm(final Feed feed, final FeedEntity feedEntity){
+        Realm realm = getRealmInstance();
+
+        try{
+            realm.beginTransaction();
+
+            feedEntity.setAuthor(feed.getAuthor());
+            feedEntity.setTitle(feed.getTitle());
+            feedEntity.setDescription(feed.getDescription());
+            feedEntity.setLink(feed.getLink());
+            feedEntity.setImage(feed.getImageLink());
+            feedEntity.setDate(feed.getPublicationDate());
+            feedEntity.setCopyright(feed.getCopyright());
+
+            realm.copyToRealmOrUpdate(feedEntity);
+            realm.commitTransaction();
+        }finally{
+            realm.close();
+        }
+    }
+
+    private void copyItemsToRealm(final List<? extends Item> items, final FeedEntity feedEntity){
+        Realm realm = getRealmInstance();
+
+        try{
             long feedId = feedEntity.getId();
 
+            for(Item item : items){
+                String itemId = item.getId();
+
+                ItemEntity itemEntity = realm.where(ItemEntity.class)
+                    .equalTo("feedId", feedId)
+                    .equalTo("itemId", itemId)
+                    .findFirst();
+
+                realm.beginTransaction();
+
+                // it is new item entity in realm
+                if(itemEntity == null){
+                    itemEntity = new ItemEntity();
+
+                    itemEntity.autoIncrementId(realm);
+
+                    itemEntity.setFeedId(feedId);
+                    itemEntity.setItemId(itemId);
+                }
+
+                itemEntity.setAuthor(item.getAuthor());
+                itemEntity.setTitle(item.getTitle());
+                itemEntity.setDescription(item.getDescription());
+                itemEntity.setLink(item.getLink());
+                itemEntity.setImage(item.getImageLink());
+                itemEntity.setDate(item.getPublicationDate());
+
+                realm.copyToRealmOrUpdate(itemEntity);
+                realm.commitTransaction();
+            }
+        }finally{
+            realm.close();
+        }
+    }
+
+    private List<FeedEntity> getFeedEntities(){
+        Realm realm = getRealmInstance();
+
+        RealmResults<FeedEntity> feedEntities = realm.where(FeedEntity.class)
+            .findAll();
+
+        return feedEntities;
+    }
+
+    private void removeFeedEntity(final long id){
+        Realm realm = getRealmInstance();
+
+        try{
+            FeedEntity feedEntity = realm.where(FeedEntity.class)
+                .equalTo("id", id)
+                .findFirst();
+
+            long feedId = feedEntity.getId();
+
+            removeAllItemEntities(feedId);
+
+            feedEntity.deleteFromRealm();
+        }finally{
+            realm.close();
+        }
+    }
+
+    private void removeAllFeedEntities(){
+        Realm realm = getRealmInstance();
+
+        try{
+            RealmResults<FeedEntity> feedEntities = realm.where(FeedEntity.class)
+                .findAll();
+
+            for(FeedEntity feedEntity : feedEntities){
+                long feedId = feedEntity.getId();
+
+                removeAllItemEntities(feedId);
+            }
+
+            realm.beginTransaction();
+
+            feedEntities.deleteAllFromRealm();
+            realm.commitTransaction();
+        }finally{
+            realm.close();
+        }
+    }
+
+    private void removeAllItemEntities(final long feedId){
+        Realm realm = getRealmInstance();
+
+        try{
             RealmResults<ItemEntity> itemEntities = realm.where(ItemEntity.class)
                 .equalTo("feedId", feedId)
                 .findAll();
 
-            itemEntities.deleteAllFromRealm();
-        }
-
-        feedEntities.deleteAllFromRealm();
-    }
-
-    private void unsetAllActiveFlagInFeed(Realm realm){
-        RealmResults<FeedEntity> feedEntities = realm.where(FeedEntity.class)
-            .equalTo("isActive", true)
-            .findAll();
-
-        for(FeedEntity feedEntity : feedEntities){
             realm.beginTransaction();
 
-            feedEntity.setActive(false);
-
+            itemEntities.deleteAllFromRealm();
             realm.commitTransaction();
+        }finally{
+            realm.close();
         }
     }
 
-    private void setActiveFlagInFeed(String url, Realm realm){
-        FeedEntity feedEntity = realm.where(FeedEntity.class)
-            .equalTo("url", url)
+    private List<ItemEntity> getItemsEntities(final FeedEntity feedEntity){
+        Realm realm = getRealmInstance();
+
+        List<ItemEntity> itemEntities;
+
+        if(feedEntity != null){
+            long feedId = feedEntity.getId();
+
+            itemEntities = realm.where(ItemEntity.class)
+                .equalTo("feedId", feedId)
+                .findAll();
+        }else{
+            itemEntities = new ArrayList<>();
+        }
+
+        return itemEntities;
+    }
+
+    private ItemEntity getItemEntity(final long id){
+        Realm realm = getRealmInstance();
+
+        ItemEntity itemEntity = realm.where(ItemEntity.class)
+            .equalTo("id", id)
             .findFirst();
 
         realm.beginTransaction();
 
-        // it is new feed entity in realm
-        if(feedEntity == null){
-            feedEntity = new FeedEntity();
+        itemEntity.setReaded(true);
 
-            feedEntity.autoIncrementId(realm);
-
-            feedEntity.setUrl(url);
-        }
-
-        feedEntity.setActive(true);
-
-        realm.copyToRealmOrUpdate(feedEntity);
         realm.commitTransaction();
+
+        return itemEntity;
     }
 
 //==================================================================================================
@@ -163,72 +297,107 @@ public class RssModelImpl implements RssModel {
 //==================================================================================================
 
     @Override
-    public String getUrl(){
-        String url = null;
-        Realm realm = getInstance();
-
-        FeedEntity feedEntity = realm.where(FeedEntity.class)
-            .equalTo("isActive", true)
-            .findFirst();
-
-        if(feedEntity != null){
-            url = feedEntity.getUrl();
-        }
-
-        return url;
-    }
-
-    @Override
-    public void setUrl(String url){
-        Realm realm = getInstance();
-
-        unsetAllActiveFlagInFeed(realm);
-        setActiveFlagInFeed(url, realm);
-    }
-
-    @Override
-    public Observable<Void> updateFeed(){
+    public Observable<FeedEntity> getFeed(){
         return Observable.create(subscriber -> {
-            Realm realm = getInstance();
-
             try{
-                String url = getUrl();
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder().url(url).build();
-                Response response = client.newCall(request).execute();
-                InputStream stream = response.body().byteStream();
+                FeedEntity feedEntity = getActiveFeedEntity();
 
-                Feed feed = EarlParser.parseOrThrow(stream, 0);
-
-                long feedId = copyFeedToRealm(feed, url, realm);
-
-                for(Item item : feed.getItems()){
-                    copyItemToRealm(item, feedId, realm);
-                }
-
-                subscriber.onNext(null);
+                subscriber.onNext(feedEntity);
                 subscriber.onCompleted();
-            }catch(NullPointerException | IOException | XmlPullParserException | DataFormatException error){
+            }catch(Throwable error){
                 subscriber.onError(error);
             }
-        }).compose(ObservableTransformer.applySchedulers());
+        });
     }
 
     @Override
-    public Observable<RealmResults<ItemEntity>> getFeed(){
+    public Observable<Void> setFeed(final String url){
         return Observable.create(subscriber -> {
-            Realm realm = getInstance();
-
             try{
-                String url = getUrl();
-                FeedEntity feedEntity = realm.where(FeedEntity.class)
-                    .equalTo("url", url)
-                    .findFirst();
-                long feedId = feedEntity.getId();
+                clearActiveFlagFromAllFeedEntities();
+                setActiveFlagForFeedEntity(url);
 
-                RealmResults<ItemEntity> itemEntities = realm.where(ItemEntity.class)
-                    .equalTo("feedId", feedId)
-                    .findAll();
+                subscriber.onCompleted();
+            }catch(Throwable error){
+                subscriber.onError(error);
+            }
+        });
+    }
+
+    @Override
+    public Observable<Boolean> getUpdateFeed(){
+        return Observable.create(subscriber -> {
+            try{
+                FeedEntity feedEntity = getActiveFeedEntity();
+
+                if(feedEntity != null){
+                    String url = feedEntity.getUrl();
+                    InputStream stream = getFeedStream(url);
+                    Feed feed = EarlParser.parseOrThrow(stream, 0);
+                    copyFeedToRealm(feed, feedEntity);
+                    copyItemsToRealm(feed.getItems(), feedEntity);
+
+                    subscriber.onNext(true);
+                }else{
+                    subscriber.onNext(false);
+                }
+
+                feedEntity = getActiveFeedEntity();
+
+                subscriber.onCompleted();
+            }catch(IOException | XmlPullParserException | DataFormatException error){
+                subscriber.onError(error);
+            }
+        }).compose(ObservableTransformer.applyMainThreadScheduler());
+    }
+
+    @Override
+    public Observable<List<FeedEntity>> getFeedsList(){
+        return Observable.create(subscriber -> {
+            try{
+                List<FeedEntity> feedEntities = getFeedEntities();
+
+                subscriber.onNext(feedEntities);
+                subscriber.onCompleted();
+            }catch(Throwable error){
+                subscriber.onError(error);
+            }
+        });
+    }
+
+    @Override
+    public Observable<Void> removeFeed(final long id){
+        return Observable.create(subscriber -> {
+            try{
+                removeFeedEntity(id);
+
+                subscriber.onCompleted();
+            }catch(Throwable error){
+                subscriber.onError(error);
+            }
+        });
+    }
+
+    @Override
+    public Observable<Void> removeAllFeeds(){
+        return Observable.create(subscriber -> {
+            try{
+                removeAllFeedEntities();
+
+                subscriber.onCompleted();
+            }catch(Throwable error){
+                subscriber.onError(error);
+            }
+        });
+    }
+
+    @Override
+    public Observable<List<ItemEntity>> getItemsList(){
+        return Observable.create(subscriber -> {
+            try{
+                FeedEntity feedEntity = getActiveFeedEntity();
+
+                List<ItemEntity> itemEntities = getItemsEntities(feedEntity);
 
                 subscriber.onNext(itemEntities);
                 subscriber.onCompleted();
@@ -239,20 +408,10 @@ public class RssModelImpl implements RssModel {
     }
 
     @Override
-    public Observable<ItemEntity> getNews(long id){
+    public Observable<ItemEntity> getItem(final long id){
         return Observable.create(subscriber -> {
-            Realm realm = getInstance();
-
             try{
-                ItemEntity itemEntity = realm.where(ItemEntity.class)
-                    .equalTo("id", id)
-                    .findFirst();
-
-                realm.beginTransaction();
-
-                itemEntity.setReaded(true);
-
-                realm.commitTransaction();
+                ItemEntity itemEntity = getItemEntity(id);
 
                 subscriber.onNext(itemEntity);
                 subscriber.onCompleted();

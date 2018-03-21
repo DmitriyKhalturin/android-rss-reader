64b68cbaf514ee2f1b04f219d3efe63ec72bd38c
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2018-03-21 18:14:44 +0300

    add Navigator class

---
+++ presentation/src/main/java/com/khalturin/dmitriy/presentation/navigator/Navigator.java
@@ -0,0 +1,9 @@
+package com.khalturin.dmitriy.presentation.navigator;
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for android-rss-reader on 21.03.18 18:10.
+ */
+
+public class Navigator {
+}
